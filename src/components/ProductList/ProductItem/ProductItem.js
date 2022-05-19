18f435039976a8296fdb172684a551b56ce57c9c
Tatyna Kubatova <tatynakubatova74@gmail.com>
2022-05-19 18:13:45 +0600

    Add new product components

---
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import classes from './ProductItem.module.css';
+
+const ProductItem = ({ product }) => {
+  return ( <div className={classes.ProductItem}>
+    <img src={product.image}/>
+    <div>
+      <Link to={product.path}>{product.title}</Link>
+      <div className={classes.discount}>{product.discount}</div>
+      <div className={classes.price}>{product.price}</div>
+    </div>
+  </div> );
+}
+
+export default ProductItem;
--- src/pages/Women.js
+++ src/pages/Women.js
@@ -1,12 +1,39 @@
 import Header from "../components/Header/Header";
 import headerImage from '../assets/women.gif'
+import ProductList from "../components/ProductList/ProductList";
+
+import productImage from '../assets/jeans.webp';
 
 const Women = () => {
+  const products = {
+    j: {
+      image: productImage,
+      path: '/',
+      price: 10,
+      title: 'Jeans',
+      discount: 50
+    },
+    s: {
+      image: productImage,
+      path: '/',
+      price: 10,
+      title: 'Shirt',
+      discount: 50
+    },
+    t: {
+      image: productImage,
+      path: '/',
+      price: 10,
+      title: 'T-shirt',
+      discount: 50
+    },
+  }
   return (
     <>
-    <Header image={headerImage}></Header>
+      <Header image={headerImage}></Header>
+      <ProductList products={products} />
     </>
-   );
+  );
 }
 
 export default Women;
